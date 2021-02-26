96176188090252e63a4485c54bc122c54bacbcdc
ryoherisson <ryhizw@gmail.com>
2021-02-26 09:22:17 +0900

    prog

--- src/eigen/prog1.cpp
+++ src/eigen/prog1.cpp
@@ -11,5 +11,17 @@ int main() {
     MatrixXd C = A.transpose();
 
     std::cout << C * A << std::endl << (A * v).transpose() << std::endl;
+    std::cout << "---------" << std::endl;
+
+    MatrixXd D(2, 4);
+    D << 2, 3, 4, 5,
+         6, 7, 8, 9;
+    MatrixXd E = MatrixXd::Constant(2, 4, 2);
+
+    std::cout << D << std::endl;
+    std::cout << D(1, 0) << std::endl << D.rowwise().mean() << std::endl;
+    std::cout << D.array() + E.array() << std::endl << D.array() * E.array() << std::endl;
+
+
     return 0;
 }
